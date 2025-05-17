976048c20514aa2128067a9eedc39f1ac5b2facd
pehtoorismies <pehtorismies@gmail.com>
2025-05-17 21:56:11 +0300

    ci(api): create d1 db

--- apps/api/src/lib/create-app.ts
+++ apps/api/src/lib/create-app.ts
@@ -4,7 +4,7 @@ import notFound from 'stoker/middlewares/not-found'
 import onError from 'stoker/middlewares/on-error'
 import { defaultHook } from 'stoker/openapi'
 import type { AppBindings, AppOpenAPI } from '~/lib/types'
-import { apiKeyMiddleware } from '~/middleware/api-key'
+// import { apiKeyMiddleware } from '~/middleware/api-key'
 import { pinoLogger } from '~/middleware/pino-logger'
 
 export const createRouter = () => {
@@ -18,7 +18,7 @@ export const createApp = () => {
   const app = createRouter()
   app.use(requestId())
   app.use(pinoLogger())
-  app.use(apiKeyMiddleware('Kissa'))
+  // app.use(apiKeyMiddleware('Kissa'))
   app.notFound(notFound)
   app.onError(onError)
   // SMELL: this is a bit of a hack to get the types right
